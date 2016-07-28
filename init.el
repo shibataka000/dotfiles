35d9d6abd848673727e4998de21ca69684f999bc
shibataka000 <chise.alter.pasta@gmail.com>
2016-07-28 23:14:22 +0900

    Add init.el.

---
+++ init.el
@@ -0,0 +1,29 @@
+;; el-get
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+(add-to-list 'load-path (locate-user-emacs-file "el-get"))
+(require 'el-get)
+(setq el-get-dir (locate-user-emacs-file ""))
+
+;; Install package
+(el-get-bundle auto-complete)
+(el-get-bundle markdown-mode)
+(el-get-bundle go-mode)
+(el-get-bundle scala-mode2)
+(el-get-bundle haskell-mode)
+(el-get-bundle python-pep8)
+
+;; Set key-bind
+(define-key global-map "\C-x\C-b" 'electric-buffer-list)
+(define-key global-map "\C-cr" 'replace-string)
+(define-key global-map "\C-c8" 'pep8)
+(define-key global-map "\C-cp" 'markdown-preview)
+
+;; Don't create backup file
+(setq make-backup-files nil)
+
+;; Dockerfile
+(setq auto-mode-alist (cons '("Dockerfile" . conf-mode) auto-mode-alist))
+
+;; go-mode
+(add-hook 'go-mode-hook '(lambda () (setq tab-width 4)))
