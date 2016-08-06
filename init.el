c14e32f7e8f8b0b091a7d7fe0c72412d47c09421
shibataka000 <chise.alter.pasta@gmail.com>
2016-08-06 13:58:32 +0900

    Add configure about flymake to check pep8 automatically.

--- init.el
+++ init.el
@@ -22,3 +22,21 @@
 
 ;; Dockerfile
 (setq auto-mode-alist (cons '("Dockerfile" . conf-mode) auto-mode-alist))
+
+;; Python
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+		       'flymake-create-temp-inplace))
+	   (local-file (file-relative-name
+			temp-file
+			(file-name-directory buffer-file-name))))
+      (list "/usr/local/bin/pep8"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+	       '("\\.py\\'" flymake-pyflakes-init)))
+(defun flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+(add-hook 'post-command-hook 'flymake-show-help)
