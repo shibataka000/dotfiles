36fe8d3c3b4776770912b041e12168efddbbbeee
Takao Shibata <chise.alter.pasta@gmail.com>
2023-04-20 22:55:00 +0900

    add kubectl_complete-argo-workflows

--- .kubectl-plugin-completion/generate.sh
+++ .kubectl-plugin-completion/generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for plugin in $(kubectl krew list) "argo rollouts"
+for plugin in $(kubectl krew list) "argo rollouts" "argo workflows"
 do
     kubectl_plugin="kubectl-$(echo $plugin | sed -e 's/-/_/' -e 's/ /-/')"
     kubectl_plugin_completion="kubectl_complete-$(echo $plugin | sed -e 's/-/_/' -e 's/ /-/')"
 
-    $kubectl_plugin __complete "" > /dev/null 2>&1
+    $kubectl_plugin __complete "--" > /dev/null 2>&1
     if [ $? == 0 ]; then
         echo "#!/usr/bin/env sh" > $kubectl_plugin_completion
         echo "$kubectl_plugin __complete \"\$@\"" >> $kubectl_plugin_completion
