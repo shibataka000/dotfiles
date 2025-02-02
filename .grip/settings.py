f2d0b90b468766dcf9cc94a4d370a8c5ec8d08d8
Takao Shibata <chise.alter.pasta@gmail.com>
2025-02-02 13:20:21 +0900

    add github-markdown.css to grip style urls

--- .grip/settings.py
+++ .grip/settings.py
@@ -2,3 +2,4 @@ import subprocess
 
 PASSWORD = subprocess.run(["gh", "auth", "token"], capture_output=True, text=True, check=True).stdout.strip()
 PORT = 8000
+STYLE_URLS = ["https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown.css"]
